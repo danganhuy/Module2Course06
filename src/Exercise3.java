24e9be863e643a85721b5261acf474f5f9c6ebf2
AnHuy <92672519+danganhuy@users.noreply.github.com>
2024-11-08 09:42:09 +0700

    Exercise 3 finished

---
+++ src/Exercise3.java
@@ -0,0 +1,9 @@
+public class Exercise3 {
+    public static void main(String[] args) {
+        Point2D a = new Point2D(8, 2);
+        Point3D b = new Point3D(2, 3, 5);
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
